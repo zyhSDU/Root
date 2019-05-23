57a95c1b287d92584bafd8cc3a26920daab6b4e4
zyh <1114130407@qq.com>
2019-05-23 15:51:16 +0800

    add MySQLUtil

---
+++ src/main/java/util/MySQLUtil.kt
@@ -0,0 +1,30 @@
+package util
+
+import java.sql.DriverManager
+
+object MySQLUtil{
+    private val JDBC_DRIVER = "com.mysql.cj.jdbc.Driver"
+    private val DB_URL = "jdbc:mysql://localhost:3306/runoob?useUnicode=true&characterEncoding=utf8&serverTimezone=UTC"
+    private val USER = "root"
+    private val PASS = "167943"
+    fun connect(){
+        Class.forName(JDBC_DRIVER)
+        val conn= DriverManager.getConnection(DB_URL, USER,PASS)
+        val stmt= conn!!.createStatement()
+        val sql = "SELECT id, name, url FROM websites"
+        val rs = stmt!!.executeQuery(sql)
+        // 展开结果集数据库
+        while (rs.next()) {
+            // 通过字段检索
+            val id = rs.getInt("id")
+            val name = rs.getString("name")
+            val url = rs.getString("url")
+            // 输出数据
+            print("ID: $id")
+            print(", 站点名称: $name")
+            print(", 站点 URL: $url")
+            print("\n")
+        }
+    }
+}
+
